f7f040248b7c4495f47c27b1b661c11935507588
Monika N <monika258@gmail.com>
2020-07-15 13:47:17 +0200

    main, basic view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className='app'>
+                <header>
+                    <div className='wrapper'>
+                      <h1>Potluck Party Planner</h1>
+                    </div>
+                </header>
+              <div className='container'>
+                <section className='add-item'>
+                  <form>
+                    <input type="text" name='username' placeholder='Your name?'/>
+                    <input type="text" name='currentItem' placeholder='WHat are you bringing?'/>
+                    <button>Add Item</button>
+                  </form>
+                </section>
+              </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD2bVUnvMYuaRZddo6YpYmImpEQn6KYdEw",
+    authDomain: "ppp-potluck-party-planner.firebaseapp.com",
+    databaseURL: "https://ppp-potluck-party-planner.firebaseio.com",
+    projectId: "ppp-potluck-party-planner",
+    storageBucket: "ppp-potluck-party-planner.appspot.com",
+    messagingSenderId: "222519728165",
+    appId: "1:222519728165:web:0015afb799600f10d687cc"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
